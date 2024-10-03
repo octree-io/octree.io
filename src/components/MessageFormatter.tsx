0beaf0816d3dd153bf0c202a13e1ead2cc5ce95c
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2024-10-03 09:14:01 -0500

    feat: Add support for url links

--- src/components/MessageFormatter.tsx
+++ src/components/MessageFormatter.tsx
@@ -1,12 +1,26 @@
 import { Fragment, memo } from "react"
 
-export const MessageFormatter = memo(({ message }: { message: string }) => (
-  <div>
-    {message.split("\n").map((line, index) => (
-      <Fragment key={index}>
-        {line}
-        <br />
-      </Fragment>
-    ))}
-  </div>
-));
+const urlRegex = /(https?:\/\/[^\s]+|www\.[^\s]+)/g;
+
+export const MessageFormatter = memo(({ message }: { message: string }) => {
+  const parts = message.split(urlRegex);
+
+  return (
+    <div>
+      {parts.map((part, index) => {
+        if (urlRegex.test(part)) {
+          const url = part.startsWith("http") ? part : `http://${part}`;
+          return (
+            <Fragment key={index}>
+              <a href={url} target="_blank" rel="noopener noreferrer">
+                {part}
+              </a>
+            </Fragment>
+          );
+        }
+
+        return <Fragment key={index}>{part}</Fragment>;
+      })}
+    </div>
+  );
+});

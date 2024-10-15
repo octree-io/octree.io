cb90a7f14061f059189f95b706e5577d5d133776
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2024-10-15 18:24:05 -0500

    chore: Update supported languages list after harness rework

--- src/views/Landing/Landing.tsx
+++ src/views/Landing/Landing.tsx
@@ -31,7 +31,7 @@ const Landing = () => {
 
             <div className="landing-hero-step">
               <h2>Select a language</h2>
-              <p>You can select up to 8 different languages. We support Python, C++, Java, C#, Ruby, Go, Rust, OCaml and more coming in the future!</p>
+              <p>You can select up to 7 different languages. We support Python, C++, Java, C#, Ruby, JavaScript and TypeScript. More coming in the future!</p>
             </div>
 
             <div className="landing-hero-step">

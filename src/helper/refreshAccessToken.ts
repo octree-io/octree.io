16484c3293193742f4dfe8cf4a66ccb0d7ff04df
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2024-10-18 18:54:34 -0500

    fix: Trigger a logout if max retry attempts reached

--- src/helper/refreshAccessToken.ts
+++ src/helper/refreshAccessToken.ts
@@ -1,7 +1,7 @@
 import apiClient, { TokenExpiredError } from "../client/APIClient";
 
 export const refreshAccessToken = async (): Promise<string | false> => {
-  let retries = 3;
+  let retries = 5;
   let delay = 1000;
 
   for (let attempt = 1; attempt <= retries; attempt++) {
@@ -18,6 +18,10 @@ export const refreshAccessToken = async (): Promise<string | false> => {
       }
 
       if (attempt === retries) {
+        // Retrying the max amount of times likely means that the token is stale and/or invalid
+        // Removing the token should trigger a re-login
+        console.log("[refreshAccessToken] Max retries reached, deleting token");
+        localStorage.removeItem("token");
         return false;
       }
 

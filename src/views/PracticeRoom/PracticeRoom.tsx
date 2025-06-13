a6a692c9d1b551a04747ce36a435f07962a5eba8
Nitin Ankad <46038298+nitinankad@users.noreply.github.com>
2025-06-12 23:31:46 -0500

    minor: Remove right margin for Leetcode URL input

--- src/views/PracticeRoom/PracticeRoom.tsx
+++ src/views/PracticeRoom/PracticeRoom.tsx
@@ -272,7 +272,7 @@ const PracticeRoom = () => {
             <div className="flex items-center mb-4">
               <input
                 type="text"
-                className={`flex-grow mr-2 px-4 py-2 rounded-l ${darkMode ? 'bg-gray-800 border-gray-700 text-gray-100' : 'bg-white border-gray-300 text-gray-900'} border focus:outline-none focus:ring-2 focus:ring-purple-600`}
+                className={`flex-grow px-4 py-2 rounded-l ${darkMode ? 'bg-gray-800 border-gray-700 text-gray-100' : 'bg-white border-gray-300 text-gray-900'} border focus:outline-none focus:ring-2 focus:ring-purple-600`}
                 placeholder="Enter Leetcode URL"
                 onChange={(e) => setLeetcodeUrl(e.target.value)}
               />
